6877967e8b352cd1493f66ca76a05fc8b39186fc
Eli Finer <eli.finer@gmail.com>
2014-01-10 02:55:21 +0200

    9 covers per A4 page

--- make_html.py
+++ make_html.py
@@ -1,4 +1,4 @@
 import glob
 with open('index.html', 'w') as f:
     for filename in glob.glob('covers/*.jpg'):
-        print >>f, '<img style="height:250px; width:165px" src="%s">' % filename
+        print >>f, '<img style="height:320px; width:215px" src="%s">' % filename
